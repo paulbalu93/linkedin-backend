606625074f6b79494e47b22bd34c3285fa2e8aba
Can Soylu <hsyncn17@gmail.com>
2021-05-10 12:58:46 +0300

    added experience schema

--- src/server.js
+++ src/server.js
@@ -1,22 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import profileRoutes from './services/profile/index.js';
-import listEndPoints from 'express-list-endpoints';
+import express from "express";
+import mongoose from "mongoose";
+import profileRoutes from "./services/profile/index.js";
+import listEndPoints from "express-list-endpoints";
 // import postRoutes from './services/post/index.js';
-// import experienceRoutes from './services/experience/index.js'
+import experienceRoutes from "./services/experience/index.js";
 const server = express();
 
 const port = process.env.PORT;
 
 server.use(express.json());
-server.use('/profile', profileRoutes);
-// server.use('/books', booksRoutes);
+server.use("/profile", profileRoutes);
+server.use("/experience", experienceRoutes);
 console.log(listEndPoints(server));
 
 mongoose
-	.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-	.then(() =>
-		server.listen(port, () => {
-			console.log('sever is running on port', port);
-		})
-	);
+  .connect(process.env.MONGO_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() =>
+    server.listen(port, () => {
+      console.log("sever is running on port", port);
+    })
+  );
---
+++ src/services/experience/schema.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const ExperienceSchema = new Schema(
+  {
+    role: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    area: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Experience", ExperienceSchema);
